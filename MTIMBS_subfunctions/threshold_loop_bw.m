1c1c02308719b3df222bf548be94260664fbff35
yildi <jslivka@berkeley.edu>
2024-10-25 11:04:32 -0700

    merged ridge finding and bwthreshold (beta)

--- MTIMBS_subfunctions/threshold_loop_bw.m
+++ MTIMBS_subfunctions/threshold_loop_bw.m
@@ -59,12 +59,74 @@ while loop_thresh == 'n' %while loop isn't broken
         return
     end
     
-    % [tempx, tempy] = find(image > ridge_threshold);
-    
+    % simple raw intensity method
+    %       Pros: includes more smaller MTs
+    %       Cons: Can cause arbitrary spikes and misfitting
+
     % make it just a basic line of max intensities
     Mrdg = bwmorph(image > ridge_threshold, 'clean');
     Mrdg = bwmorph(Mrdg, 'thin', 'inf');
     [tempx, tempy] = find(Mrdg);
+
+    
+    % A breakdown using sliding windows of maxima (JS edit 2024/10/25)
+    %       Pros: Decreases background and spikes
+    %       Cons: Might miss smaller MTs
+    %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%                  R I D G E S  &  R I V E R S
+    %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    I = image;
+    [m n] 	= size(I);
+    %% -----    Parameters
+    s       = 1 ;       % search radius for finding if the maximum value is good, if overexposed then should be higher
+    minCtr  = 0.05;
+    %% ------------------   Subimages & Indices     -----------------
+    rr	= s+1:m-s;      rrN	= rr-s;     rrS = rr+s;
+    cc 	= s+1:n-s;      ccE = cc+s;     ccW = cc-s;
+    CEN	= I(rr,	cc);                        % center
+    NN 	= I(rrN,cc);   	SS = I(rrS,cc);  	% north, south
+    EE  = I(rr, ccE);  	WW = I(rr, ccW);   	% east, west
+    NE  = I(rrN,ccE);  	SE = I(rrS,ccE);   	% north east, south east
+    SW  = I(rrS,ccW);  	NW = I(rrN,ccW);   	% south west, north west
+    %% =============	EXTREMA Along Axes & Diagonals
+    % --- Maxima (counter-clockwise) just by neighbor comparison determined by s ---
+    MX1     = padarray(CEN>NN & CEN>SS, [s s]);  % max north-south
+    MX2     = padarray(CEN>NW & CEN>SE, [s s]);  % max diag 1
+    MX3     = padarray(CEN>WW & CEN>EE, [s s]);  % max west-east
+    MX4     = padarray(CEN>NE & CEN>SW, [s s]);  % max diag 2
+    % --- Minima
+    MN1     = padarray(CEN<NN & CEN<SS, [s s]);  % min north-south
+    MN2     = padarray(CEN<NE & CEN<SW, [s s]);  % min diag 1
+    MN3     = padarray(CEN<WW & CEN<EE, [s s]);  % min west-east
+    MN4     = padarray(CEN<NW & CEN<SE, [s s]);  % min diag 2
+    Cmax	= uint8(MX1+MX2+MX3+MX4);      % map of maxima count
+    Cmin    = uint8(MN1+MN2+MN3+MN4);      % map of minima count
+    %% =============	Suppress Low RR Contrast
+    R           = colfilt(I,[2 2]+s, 'sliding', @range);    % range image
+    CtrXtr      = R(logical(Cmax) | logical(Cmin));         % [nExtrema 1]
+    thrXtr     	= max(CtrXtr)*minCtr;       % threshold for low contrast
+    Blow       	= R < thrXtr;               % map with low contrast pixels
+    Cmax(Blow) 	= 0;                        % eliminate low contrast
+    %% =============	Ridge/River Maps
+    Mrdg       	= Cmax >= 2; % more than 2 maxima in one of the four determined directions
+    Mrdg      	= bwmorph(Mrdg, 'clean');
+    Mrdg     	= bwmorph(Mrdg, 'thin', 'inf');
+    
+    [RwRdg ClRdg]   = find(Mrdg);
+    
+    % plot(RwRdg, ClRdg, 'g.',"tag","threshold_ridge")
+    % plot(RwRiv, ClRiv, 'b.',"tag","threshold_ridge")
+
+    for j = length(RwRdg):-1:1
+        [xidx,~] = find(abs(RwRdg - RwRdg(j)) < search_radius);
+        [yidx,~] = find(abs(ClRdg - ClRdg(j)) < search_radius);
+        idx = intersect(xidx,yidx);
+        if length(idx) < num_points
+            RwRdg(j) = []; ClRdg(j) = [];
+        end
+    end
+    
+    % plot(RwRdg, ClRdg, 'g.',"tag","threshold_ridge")
     
     % JS Edit 2022/11/11 remove small clusters to deal with noisier
     % aggregate backgrounds
@@ -78,9 +140,23 @@ while loop_thresh == 'n' %while loop isn't broken
         end
     end
     
+    % Use the combination of these two methods to determine whether the MT
+    % is real or not (or just background). This removes a number of false
+    % positives from ridge while avoiding spikes from pure bwthreshold
+    for j = length(RwRdg):-1:1
+        [xidx,~] = find(abs(tempx - RwRdg(j)) < search_radius);
+        [yidx,~] = find(abs(tempy - ClRdg(j)) < search_radius);
+        idx = intersect(xidx,yidx);
+        if length(idx) < num_points
+            RwRdg(j) = []; ClRdg(j) = [];
+        end
+    end
     
     figure(1);
-    plot(tempx,tempy,'r.',"tag","threshold_ridge"); %plot the thresholded points as red
+    tempx = RwRdg;
+    tempy = ClRdg;
+    % plot(tempx,tempy,'r.',"tag","threshold_ridge"); %plot the thresholded points as red
+    plot(tempx, tempy, 'r.',"tag","threshold_ridge")
     
     %% To make it so that one can delete individual points by point and click
     % JS Edit 220309
